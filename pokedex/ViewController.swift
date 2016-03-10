dc296c511447eb22826119798857edf981aa3b9e
Vu Dang <jublusion5@gmail.com>
2016-03-10 17:51:13 -0600

    Create test cells to see images show up on the storyboard

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -8,19 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var collection: UICollectionView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        collection.delegate = self
+        collection.dataSource = self
     }
     
-
-
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        if let cell = collection.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell
+            {
+                let pokemon = Pokemon(name: "test", pokedexId: indexPath.row + 1)
+                cell.configureCell(pokemon)
+                
+                return cell
+            }
+        else
+            {
+                return UICollectionViewCell()
+            }
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
+    {
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return 718
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
+    {
+        return CGSizeMake(105, 105)
+    }
+    
 }
 
