450fddd1eb3db7754a9a8701f6cc95475a659305
Vu Dang <jublusion5@gmail.com>
2016-03-28 21:22:46 -0500

    applied all our parsed data into the pokemonDetailsVC

--- pokedex/pokemonDetailVC.swift
+++ pokedex/pokemonDetailVC.swift
@@ -21,6 +21,8 @@ class pokemonDetailVC: UIViewController
     @IBOutlet weak var weightLbl: UILabel!
     @IBOutlet weak var defenseLbl: UILabel!
     @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
     
     
     
@@ -31,14 +33,39 @@ class pokemonDetailVC: UIViewController
     {
         super.viewDidLoad()
         
+        let img = UIImage(named: "\(pokemon.pokedexId)")
         pokemonNameLbl.text = pokemon.name.capitalizedString
-        mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        mainImg.image = img
+        currentEvoImg.image = img
         pokedexLbl.text = "\(pokemon.pokedexId)"
         
         pokemon.downloadPokemonDetails
         { () -> () in
             //this will be called after download is done
+            self.updateData()
         }
+    }
+    
+    func updateData()
+    {
+        descriptionLbl.text = pokemon.description
+        typeLbl.text = pokemon.type
+        heightLbl.text = pokemon.height
+        attackLbl.text = pokemon.attack
+        weightLbl.text = pokemon.weight
+        defenseLbl.text = pokemon.defense
+        
+        if pokemon.nextEvolutionText == ""
+            {
+                evoLbl.text = "NO NEXT EVOLUTION"
+                nextEvoImg.hidden = true
+            }
+        else
+            {
+                nextEvoImg.image = UIImage(named: "\(pokemon.nextEvolutionId)")
+                nextEvoImg.hidden = false
+                evoLbl.text = "Next evolution: \(pokemon.nextEvolutionText) (\(pokemon.nextEvolutionLvl))"
+            }
     }
 
     @IBAction func backBtnPressed(sender: AnyObject)
