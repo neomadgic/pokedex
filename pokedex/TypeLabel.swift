721de19f34dfcbf34eb6434bea7dff39e6d7bb4a
Vu Dang <jublusion5@gmail.com>
2016-03-29 20:16:49 -0500

    Created an extension for UIColor to convert into hexadecimal, finished changeColor() so that every type will change color

--- pokedex/TypeLabel.swift
+++ pokedex/TypeLabel.swift
@@ -14,23 +14,52 @@ class TypeLabel: UILabel
     
     func changeColor()
     {
-        identifyType("Fire", redFloat: 1.0, greenFloat: 0.0, blueFloat: 0.0)
-        identifyType("Grass", redFloat: 0.0, greenFloat: 1.0, blueFloat: 0.0)
-        identifyType("Water", redFloat: 0.0, greenFloat: 0.0, blueFloat: 1.0)
-        identifyType("Electric", redFloat: 1.0, greenFloat: 1.0, blueFloat: 0.0)
-        
-        
+        let attributedString = NSMutableAttributedString(string: self.text!)
+        identifyType("Normal", attributedString: attributedString, hexCode: 0xA8A77A)
+        identifyType("Fire", attributedString: attributedString, hexCode: 0xEE8130)
+        identifyType("Water", attributedString: attributedString, hexCode: 0x6390F0)
+        identifyType("Electric", attributedString: attributedString, hexCode: 0xF7D02C)
+        identifyType("Grass", attributedString: attributedString, hexCode: 0x7AC74C)
+        identifyType("Ice", attributedString: attributedString, hexCode: 0x96D9D6)
+        identifyType("Fighting", attributedString: attributedString, hexCode: 0xC22E28)
+        identifyType("Poison", attributedString: attributedString, hexCode: 0xA33EA1)
+        identifyType("Ground", attributedString: attributedString, hexCode: 0xE2BF65)
+        identifyType("Flying", attributedString: attributedString, hexCode: 0xA98FF3)
+        identifyType("Psychic", attributedString: attributedString, hexCode: 0xF95587)
+        identifyType("Bug", attributedString: attributedString, hexCode: 0xA6B91A)
+        identifyType("Rock", attributedString: attributedString, hexCode: 0xB6A136)
+        identifyType("Ghost", attributedString: attributedString, hexCode: 0x735797)
+        identifyType("Dragon", attributedString: attributedString, hexCode: 0x6F35FC)
+        identifyType("Dark", attributedString: attributedString, hexCode: 0x705746)
+        identifyType("Steel", attributedString: attributedString, hexCode: 0xB7B7CE)
+        identifyType("Fairy", attributedString: attributedString, hexCode: 0xD685AD)
         
     }
     
-    func identifyType(type: String, redFloat: CGFloat, greenFloat: CGFloat, blueFloat: CGFloat)
+    func identifyType(type: String, attributedString: NSMutableAttributedString, hexCode: Int)
     {
         if (self.text?.rangeOfString(type) != nil)
             {
                 let range = (self.text! as NSString).rangeOfString(type)
-                let attributedString = NSMutableAttributedString(string: self.text!)
-                attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor(red: redFloat, green: greenFloat, blue: blueFloat, alpha: 1.0), range: range)
+                attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor(netHex: hexCode), range: range)
                 self.attributedText = attributedString;
             }
+    }
+}
+
+extension UIColor
+{
+    convenience init(red: Int, green: Int, blue: Int)
+    {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue:CGFloat(blue)/255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex: Int)
+    {
+        self.init(red: (netHex >> 16) & 0xff, green: (netHex >> 8) & 0xff, blue: (netHex & 0xff))
     }
 }
